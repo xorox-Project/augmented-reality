5b1df4e9d1cd041cbe97a06ccfb6da224e6b3583
Gonzalo Perez Rojas <0xg@Gonzalos-MacBook-Pro.local>
2024-06-28 20:57:42 -0300

    agrega modelos

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,22 @@
 'use client';
 
 import { ARView, ARFaceMesh, ARAnchor } from "react-three-mind";
+import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+// import StaticAABB from "../public/Claveles_MarianaJU - Mariana JU.glb"
 
 export default function Home() {
+
+  const model = useGLTF ('./Azucenas_MarianaJU - Mariana JU.glb')
+  const modelRef = useRef();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    
+    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between">
+      <div className="w-full h-full">
         <ARView
           imageTargets={`/targets.mind`}
-          filterMinCF={1}
+          filterMinCF={0.1}
           filterBeta={10000}
           missTolerance={10}
           warmupTolerance={0}
@@ -16,13 +24,21 @@ export default function Home() {
           <ARAnchor
             target={0}
             >
-                        <ambientLight />
+              <primitive ref={modelRef} object={model.scene} scale={0.005} position={[-1,-1,-1]} />
+              <directionalLight color={'#fff'} shadow={true} intensity={4} position={[0,20,10]}/>
+              {/* <StaticAABB childrenRef={modelRef} helper={true} /> */}
+            {/* <mesh position={[props["position-x"],props["position-y"],props["position-z"]]}>
+              <boxGeometry args={[8, 8, 2]} />
+              <meshStandardMaterial />
+            </mesh> */}
+                        {/* <ambientLight />
 
             <pointLight position={[10, 10, 10]} />
             <mesh >
               <boxGeometry args={[0.5, 0.5, 0.5]} />
               <meshStandardMaterial color={"hotpink"} />
-            </mesh>
+            </mesh> */}
+
           </ARAnchor>
         </ARView>
         {/* <ARView
