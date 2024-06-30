d76eb28f3886caf05ffea15de33344ad6e5caea2
Gonzalo Perez Rojas <0xg@Gonzalos-MacBook-Pro.local>
2024-06-30 20:27:42 -0300

    agrega un modelo

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { ARView, ARFaceMesh, ARAnchor } from "react-three-mind";
+import { ARView, ARAnchor } from "react-three-mind";
 import { useGLTF } from "@react-three/drei";
 import { useRef } from "react";
 // import StaticAABB from "../public/Claveles_MarianaJU - Mariana JU.glb"
@@ -8,6 +8,8 @@ import { useRef } from "react";
 export default function Home() {
 
   const model = useGLTF ('./Azucenas_MarianaJU - Mariana JU.glb')
+  const model2 = useGLTF ('/turtle.glb')
+
   const modelRef = useRef();
 
   return (
@@ -15,48 +17,30 @@ export default function Home() {
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-between">
       <div className="w-full h-full">
         <ARView
-          imageTargets={`/targets.mind`}
-          filterMinCF={0.1}
-          filterBeta={10000}
-          missTolerance={10}
-          warmupTolerance={0}
+          autoplay // Automatically starts tracking once the camera stream is ready
+          flipUserCamera={false} // Prevents automatic flipping of the user camera
+          imageTargets={`/targets-ana.mind`} // URL of the generated image targets features
+          maxTrack={2} // Maximum number of targets tracked simultaneously
+          filterMinCF={0.01} // Cutoff Frequency, decrease to reduce jittering
+          filterBeta={10000} // Speed Coefficient, increase to reduce delay
+          missTolerance={10} // Number of continuous frames required for a target not being detected to be marked as lost
+          warmupTolerance={0} // Number of continuous frames required for a target being detected to be marked as found
         >
-          <ARAnchor
+{/*           <ARAnchor
             target={0}
             >
               <primitive ref={modelRef} object={model.scene} scale={0.005} position={[-1,-1,-1]} />
-              <directionalLight color={'#fff'} shadow={true} intensity={4} position={[0,20,10]}/>
-              {/* <StaticAABB childrenRef={modelRef} helper={true} /> */}
-            {/* <mesh position={[props["position-x"],props["position-y"],props["position-z"]]}>
-              <boxGeometry args={[8, 8, 2]} />
-              <meshStandardMaterial />
-            </mesh> */}
-                        {/* <ambientLight />
-
-            <pointLight position={[10, 10, 10]} />
-            <mesh >
-              <boxGeometry args={[0.5, 0.5, 0.5]} />
-              <meshStandardMaterial color={"hotpink"} />
-            </mesh> */}
-
+              <directionalLight color={'#fff'} shadow={true} intensity={1} position={[0,20,10]}/>
+              <directionalLight color={'#fff'} shadow={true} intensity={1} position={[0,-20,10]}/>
+          </ARAnchor> */}
+          <ARAnchor
+            target={0}
+            >
+              <primitive ref={modelRef} object={model2.scene} scale={1} position={[-1,-1,-1]} />
+              <directionalLight color={'#fff'} shadow={true} intensity={2} position={[0,20,10]}/>
+              <directionalLight color={'#fff'} shadow={true} intensity={2} position={[0,-20,10]}/>
           </ARAnchor>
         </ARView>
-        {/* <ARView
-            imageTargets="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/card.mind"
-            filterMinCF={1}
-            filterBeta={10000}
-            missTolerance={0}
-            warmupTolerance={0}
-          >
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <ARAnchor target={0}>
-            <mesh >
-              <boxGeometry args={[0.5, 0.5, 0.5]} />
-              <meshStandardMaterial color={"hotpink"} />
-            </mesh>
-          </ARAnchor>
-        </ARView> */}
       </div>
     </main>
   );
